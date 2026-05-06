007af3ac9e2cf32767ea93baf0d2886611ae6ac2
Illy Smith <cyphers871@proton.me>
2026-05-06 00:08:38 -0700

    added more css and formating + added seprate login page + added title bar + integrated dementia.html(does nothing)

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 // script.js
 
-document.getElementById("login").addEventListener("submit", function (e) {
-    e.preventDefault()
 
-    username = document.getElementById("username").value
-    password = document.getElementById("password").value
-    if (username == "" || password == "") {
-        msger = document.getElementById("login_msger")
-        msger.textContent = "Both a username and password are required"
-        msger.style.display = "block"
-        msger.style.color = "red"
-    }
-    else {
-        change_title("Welcome " + username)
-        document.getElementById("login").style.display = "none"
-    }
-})
 
 function change_title(title) {
     let header = document.getElementById("title")
